18e4a9fab13b0147dc0f835c8cc69e87fb0e55df
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-11-30 22:33:49 -0500

    Initialize repository

---
+++ 2018/1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("December 1st, 2018")
+
+	res := 0
+
+	fmt.Printf("Result: %v\n", res)
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/mfcochauxlaberge/advent-of-code
