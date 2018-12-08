c0b558591aefa173f134bb62a499d6338ea451c6
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-12-08 11:51:20 -0500

    Start dec 8th

---
+++ 2018/08/08.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("December 8th, 2018")
+
+	input := []byte(``)
+	// 	input := []byte(`1, 1
+	// 1, 6
+	// 8, 3
+	// 3, 4
+	// 5, 5
+	// 8, 9`)
+
+	lines := strings.Split(string(input), "\n")
+	for i := 0; i < len(lines); i++ {
+
+	}
+	fmt.Printf("Result (part 1): Unknow (tbd from the areas)\n")
+}
