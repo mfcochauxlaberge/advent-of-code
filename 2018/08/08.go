8e00028afbf4a1b250f39fa7508e2e93fbda7d6d
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-12-09 23:56:34 -0500

    Save

--- 2018/08/08.go
+++ 2018/08/08.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -11,16 +12,53 @@ func main() {
 	// input := []byte(``)
 	input := []byte(`2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`)
 
-	nums := strings.Split(string(input), "\n")
-	root := node{
-		children: []node{},
-	}
-	for i := 0; i < len(nums); i++ {
-		header:=
-	}
-	fmt.Printf("Result (part 1): Unknow (tbd from the areas)\n")
+	nums := strings.Split(string(input), " ")
+	root, _ := parseNode(nums, 0)
+	summd := countmdentries(root)
+	fmt.Printf("Result (part 1): %d\n", summd)
 }
 
 type node struct {
-	children []node
+	level    int
+	children []*node
+	metadata []int
+}
+
+func countmdentries(n *node) int {
+	sum := 0
+	for _, node := range n.children {
+		sum += countmdentries(node)
+	}
+	return sum
+}
+
+func parseNode(nums []string, level int) (*node, int) {
+	fmt.Printf("NUMS: %+v\n", nums)
+	if len(nums) < 3 {
+		panic("nums is too short")
+	}
+
+	currNode := &node{
+		level:    level,
+		children: []*node{},
+	}
+
+	var nn *node
+	end := 0
+
+	headerChildren, _ := strconv.ParseInt(nums[0], 10, 64)
+	headerMetadata, _ := strconv.ParseInt(nums[0], 10, 64)
+
+	for i := int64(0); i < headerChildren; i++ {
+		subnums := make([]string, len(nums[2:]))
+		copy(subnums, nums[2:])
+		nn, end = parseNode(subnums, level+1)
+		currNode.children = append(currNode.children, nn)
+	}
+
+	for i := int64(0); i < headerMetadata; i++ {
+
+	}
+
+	return currNode, end + int(headerMetadata)
 }
