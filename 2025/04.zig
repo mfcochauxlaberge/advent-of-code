f2efb05e3749daa570b889e40d7d5e52c9c511b2
Marc-François Cochaux-Laberge <contact@mfcl.ca>
2025-12-06 14:00:45 -0500

    Complete part 2 of day 4 (2025)

--- 2025/04.zig
+++ 2025/04.zig
@@ -10,49 +10,57 @@ pub fn main() !void {
     const data = try cwd.readFileAlloc(alloc, "04.txt", 32 * 1024);
     defer alloc.free(data);
 
-    var rolls: u64 = 0;
+    var totalRolls: u64 = 0;
 
-    var grid = std.array_list.Managed([]const u8).init(alloc);
-    defer grid.deinit();
+    var grid: [200][200]u8 = undefined;
 
-    // var lines = std.mem.tokenizeAny(u8, data, "\n");
     var lines = std.mem.tokenizeAny(u8, data, "\n");
-    while (lines.next()) |line| {
-        if (line.len == 0) continue;
-        try grid.append(line);
-    }
-
-    // Double loop to check each character
-    for (grid.items, 0..) |line, row| {
-        for (line, 0..) |char, col| {
-            if (char != '@') continue;
-
-            var count: u8 = 0;
-
-            const directions = [_][2]i32{
-                .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 },
-                .{ 0, -1 },  .{ 0, 1 },  .{ 1, -1 },
-                .{ 1, 0 },   .{ 1, 1 },
-            };
-
-            for (directions) |dir| {
-                const new_row = @as(i32, @intCast(row)) + dir[0];
-                const new_col = @as(i32, @intCast(col)) + dir[1];
-
-                if (new_row < 0 or new_row >= grid.items.len) continue;
-                if (new_col < 0 or new_col >= line.len) continue;
-
-                const c = grid.items[@intCast(new_row)][@intCast(new_col)];
-                if (c != '@') continue;
-
-                count += 1;
-            }
-
-            if (count <= 3) {
-                rolls += 1;
-            }
+    var r: usize = 0;
+    while (lines.next()) |line| : (r += 1) {
+        for (line, 0..) |char, c| {
+            grid[r][c] = char;
         }
     }
 
-    std.debug.print("Total rolls: {d}\n", .{rolls});
+    while (true) {
+        var rolls: u64 = 0;
+
+        for (grid, 0..) |line, row| {
+            for (line, 0..) |char, col| {
+                if (char != '@') continue;
+
+                var count: u8 = 0;
+
+                const directions = [_][2]i32{
+                    .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 },
+                    .{ 0, -1 },  .{ 0, 1 },  .{ 1, -1 },
+                    .{ 1, 0 },   .{ 1, 1 },
+                };
+
+                for (directions) |dir| {
+                    const new_row = @as(i32, @intCast(row)) + dir[0];
+                    const new_col = @as(i32, @intCast(col)) + dir[1];
+
+                    if (new_row < 0 or new_row >= grid.len) continue;
+                    if (new_col < 0 or new_col >= line.len) continue;
+
+                    const c = grid[@intCast(new_row)][@intCast(new_col)];
+                    if (c != '@') continue;
+
+                    count += 1;
+                }
+
+                if (count <= 3) {
+                    rolls += 1;
+                    grid[row][col] = '.';
+                }
+            }
+        }
+
+        if (rolls == 0) break;
+
+        totalRolls += rolls;
+    }
+
+    std.debug.print("Total rolls: {d}\n", .{totalRolls});
 }
